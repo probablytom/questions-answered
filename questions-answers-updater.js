bc5c9a38350b22e1f842ab164ee1627db1d8c85d
Tom Wallis <tom@tom-wallis.com>
2015-10-28 12:58:04 +0000

    Initial broken commit

---
+++ questions-answers-updater.js
@@ -0,0 +1,24 @@
+var myDataRef = new Firebase('URL GOES HERE');
+
+myDataRef.on('child_added', function(snapshot) {
+	var added_entry = snapshot.val();
+	console.log("Added question was: ");
+	console.log(added_entry);
+});
+
+myDataRef.on('child_changed', function(snapshot) {
+	var child_changed = snapshot.val();
+	console.log("Changed child was: ");
+	console.log(child_changed);
+});
+
+$("#add_question_button").click(submitQuestion(
+			$("new_question").text()
+));
+
+function submitQuestion(question_text) {
+	myDataRef.push({question_text: question_text, answer_text: null});
+}
+
+function submitAnswer(question_id, answer_text) {
+}
