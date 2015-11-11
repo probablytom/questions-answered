19d373343c4c5c509a2f98fba4a8bdcfd0b3973b
Tom Wallis <tom@tom-wallis.com>
2015-11-11 10:15:10 +0000

    Saving work so far

--- questions-answers-updater.js
+++ questions-answers-updater.js
@@ -1,24 +1,29 @@
+
 var myDataRef = new Firebase('https://sizzling-heat-3896.firebaseio.com');
 
-myDataRef.on('child_added', function(snapshot) {
-	var added_entry = snapshot.val();
-	console.log("Added question was: ");
-	console.log(added_entry);
+
+
+$(document).ready( function() {
+
+	myDataRef.on('child_added', function(snapshot) {
+		$("#questions-container").append("<h3>" + snapshot.val()["question_text"] + "</h3>");
+		console.log(snapshot.val());
+	});
+
+	myDataRef.on('child_changed', function(snapshot) {
+		var answers = snapshot.val()["answer_text"];
+		console.log(answers);
+	});
+
 });
 
-myDataRef.on('child_changed', function(snapshot) {
-	var child_changed = snapshot.val();
-	console.log("Changed child was: ");
-	console.log(child_changed);
-});
 
-$("#add_question_button").click(submitQuestion(
-			$("new_question").text()
-));
-
-function submitQuestion(question_text) {
-	myDataRef.push({question_text: question_text, answer_text: null});
+function submitQuestion() {
+	question_text = $("#new_question").val();
+	console.log(question_text);
+	myDataRef.push({question_text: question_text, answer_text: {none: null}});
 }
 
 function submitAnswer(question_id, answer_text) {
 }
+		
